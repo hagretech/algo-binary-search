21a6f2cf2bc389bc722867f9aa9e4dd589d64152
kirubel demelah <kiztopia.2020@gmail.com>
2020-11-03 16:23:07 -0500

    iterative binary serach added

--- app.py
+++ app.py
@@ -5,7 +5,23 @@ def liner_search(list, target):
             return True
     return False
 
+# iterative binary search
+
+def iterative_search(list, target):
+    low = 0
+    high = len(list) - 1
+    while high >= low:
+        mid = (low + high ) //2
+        if list[mid] == target:
+            return True
+        elif list[mid] < target:
+            low = mid +1
+        else :
+            high = mid -1
+
+    return False
 list = [1,2,3,4,5]
 target = 6
 
-print(liner_search(list, target))
+print(iterative_search(list, target))
+
